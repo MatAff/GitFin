8e6cb5da9c4e2568a4d63f73dc4636de1968b07c
MatAff <mathijsaffourtit@gmail.com>
2015-07-18 19:45:45 -0600

    Update

--- SetUpTable.py
+++ SetUpTable.py
@@ -2,4 +2,7 @@ import mysql.connector
 
 cnx = mysql.connector.connect(user='finance', password='nederland', host='localhost', database='finance')
 
+cursor = cnx.cursor()
+cursor.execute("SHOW DATABASES;")
+
 cnx.close()
