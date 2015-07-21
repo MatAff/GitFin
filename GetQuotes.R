c2b0376e2a88718545923016a4fcd44c23d6ca17
MatAff <mathijsaffourtit@gmail.com>
2015-07-20 22:57:58 -0600

    Update

--- GetQuotes.R
+++ GetQuotes.R
@@ -1,37 +1,43 @@
 
 # Load packages
-library(quantmod)
+  library(quantmod)
 
 # Input - Load symbols - Hardcoded
-selectSymbols <- c("AAPL", "ADBE", "ADI", "ADP", "ADSK", "AKAM", "ALTR", "ALXN", "AMAT", "AMGN", "AMZN", 
-  "ATVI", "AVGO", "BBBY", "BIDU", "BIIB", "BRCM", "CA", "CELG", "CERN", "CHKP", "CHRW", 
-  "CHTR", "CMCSA", "COST", "CSCO", "CTRX", "CTSH", "CTXS", "DISCA", "DLTR", "DTV", "EBAY", 
-  "EQIX", "ESRX", "EXPD", "EXPE", "FAST", "FB", "FFIV", "FISV", "FOSL", "FOXA", "GILD", 
-  "GMCR", "GOLD", "GOOG", "GRMN", "HSIC", "INTC", "INTU", "ISRG", "KLAC", "KRFT", "LBTYA", 
-  "LINTA", "LLTC", "LMCA", "MAT", "MCHP", "MDLZ", "MNST", "MSFT", "MU", "MXIM", "MYL", 
-  "NFLX", "NTAP", "NUAN", "NVDA", "ORLY", "PAYX", "PCAR", "PCLN", "QCOM", "REGN", "ROST", 
-  "SBAC", "SBUX", "SHLD", "SIAL", "SIRI", "SNDK", "SPLS", "SRCL", "STX", "SYMC", "TSLA", 
-  "TXN", "VIAB", "VOD", "VRSK", "VRTX", "WDC", "WFM", "WYNN", "XLNX", "XRAY", "YHOO")
+  selectSymbols <- c("AAPL", "ADBE", "ADI", "ADP", "ADSK", "AKAM", "ALTR", "ALXN", "AMAT", "AMGN", "AMZN", 
+    "ATVI", "AVGO", "BBBY", "BIDU", "BIIB", "BRCM", "CA", "CELG", "CERN", "CHKP", "CHRW", 
+    "CHTR", "CMCSA", "COST", "CSCO", "CTRX", "CTSH", "CTXS", "DISCA", "DLTR", "DTV", "EBAY", 
+    "EQIX", "ESRX", "EXPD", "EXPE", "FAST", "FB", "FFIV", "FISV", "FOSL", "FOXA", "GILD", 
+    "GMCR", "GOLD", "GOOG", "GRMN", "HSIC", "INTC", "INTU", "ISRG", "KLAC", "KRFT", "LBTYA", 
+    "LINTA", "LLTC", "LMCA", "MAT", "MCHP", "MDLZ", "MNST", "MSFT", "MU", "MXIM", "MYL", 
+    "NFLX", "NTAP", "NUAN", "NVDA", "ORLY", "PAYX", "PCAR", "PCLN", "QCOM", "REGN", "ROST", 
+    "SBAC", "SBUX", "SHLD", "SIAL", "SIRI", "SNDK", "SPLS", "SRCL", "STX", "SYMC", "TSLA", 
+    "TXN", "VIAB", "VOD", "VRSK", "VRTX", "WDC", "WFM", "WYNN", "XLNX", "XRAY", "YHOO")
 
 # Subset for development purposes
-selectSymbols <- selectSymbols[1:5]
+  selectSymbols <- selectSymbols[1:5]
 
 # Processing - Get quote
-quoteData <- getQuote(selectSymbols)
+  quoteData <- getQuote(selectSymbols)
 
 # Output - Print data to screen - For dev purposes
-for(shareNr in 1:length(selectSymbols)) {
-  print(paste(selectSymbols[shareNr], " - ", quoteData[shareNr, "Last"], sep=""))
-}
+  for(shareNr in 1:length(selectSymbols)) {
+    print(paste(selectSymbols[shareNr], " - ", quoteData[shareNr, "Last"], sep=""))
+  }
 
 # Write to database
-library(RMySQL)
+  library(RMySQL)
 
-mydb = dbConnect(MySQL(), user='finance', password='nederland', host='localhost')
+# Set up connection
+  mydb = dbConnect(MySQL(), user='finance', password='nederland', host='localhost')
+  on.exit(dbDisconnect(con))
 
-rs <- dbSendQuery(mydb, "SHOW DATABASES;")
+# Select database
+  rs <- dbSendQuery(mydb, "USE finance;")
 
-data <- fetch(rs, n=10)
-huh <- dbHasCompleted(rs)
-dbClearResult(mydb)
-dbDisconnect(mydb)
+# Enter data
+  query <- paste("INSERT INTO prices (ticker, price)
+    VALUES ('TEST', '99.99');")
+  rs <- dbSendQuery(mydb, query)
+
+# Disconnect
+  dbDisconnect(mydb)
