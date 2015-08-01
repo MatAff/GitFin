7094b3fb206649d7334d6677e37d590b8947bde9
MatAff <mathijsaffourtit@gmail.com>
2015-08-01 13:39:40 -0600

    GetRss

--- GetRss.R
+++ GetRss.R
@@ -1,7 +1,9 @@
 
 # Load packages
   library(XML)
+  library(lubridate)
   source("dbFunctions.R")
+  source("SourceTimeFunction.R")
 
 #################################################
 ### FUNCTIONS FOR PULLING AND PROCESSING DATA ###
@@ -51,53 +53,6 @@ ProcessTop <- function(siteName, xmltop, xpath, timestampTag, titleTag, descTag,
   return(returnData)
 }
 
-# Function to process time information
-GetTime <- function(dateTimeText) {
-  
-  # Get year
-  year <- unlist(regmatches(dateTimeText, gregexpr("201[0-9]", dateTimeText)))
-  dateTimeText <- sapply(1:length(dateTimeText), function(x) gsub(year[x], "", dateTimeText[x]))
-  #print(year); print(dateTimeText)
-  
-  # Get weekday
-  daysVec <- c("mon|tue|wed|thu|fri|sat|sun|Mon|Tue|Wed|Thu|Fri|Sat|Sun")
-  weekDay <- unlist(regmatches(dateTimeText, gregexpr(daysVec, dateTimeText)))
-  dateTimeText <- sapply(1:length(dateTimeText), function(x) gsub(weekDay[x], "", dateTimeText[x]))
-  #print(weekDay); print(dateTimeText)
-  
-  # Get time zone
-  timeZone <- unlist(regmatches(dateTimeText, gregexpr("[A-Z]{3}|-[0-9]{2}00", dateTimeText)))
-  dateTimeText <- sapply(1:length(dateTimeText), function(x) gsub(timeZone[x], "", dateTimeText[x]))
-  #print(timeZone); print(dateTimeText)
-  
-  # Get time section
-  timeText <- unlist(regmatches(dateTimeText, gregexpr("[0-2][0-9]:[0-5][0-9]:[0-5][0-9]", dateTimeText)))
-  dateTimeText <- sapply(1:length(dateTimeText), function(x) gsub(timeText[x], "", dateTimeText[x]))
-  #print(timeText); print(dateTimeText)
-  
-  # Get month tag
-  month <- regmatches(dateTimeText, gregexpr("[a-zA-Z]{3}", dateTimeText))
-  monthTags <- c("Jan", "Feb", "Mar", "Apr", "May", "Jun", "Jul", "Aug", "Sep", "Oct", "Nov", "Dec")
-  monthNrs <- c("01", "02", "03", "04", "05", "06", "07", "08", "09", "10", "11", "12")
-  for(m in 1:12) { month <- sapply(1:length(month), function(x) gsub(monthTags[m], monthNrs[m], month[x])) }
-  dateTimeText <- sapply(1:length(dateTimeText), function(x) gsub(month[x], "", dateTimeText[x]))
-  #print(month); print(dateTimeText)
-  
-  # Date
-  date <- regmatches(dateTimeText, gregexpr(" [0-9]{2} ", dateTimeText))
-  date <- gsub(" ", "", date)
-  dateTimeText <- sapply(1:length(dateTimeText), function(x) gsub(date[x], "", dateTimeText[x]))
-  #print(date); print(dateTimeText)
-  
-  # Compile date string
-  dateTimeText <- paste(year,"-",month,"-",date, " ", timeText, sep="")
-  # dateTimeText <- as.Date(dateTimeText)
-  
-  # Return dateTimeText
-  return(dateTimeText)
-  
-}
-
 #################
 ### PULL DATA ###
 #################
@@ -120,21 +75,15 @@ xmltop <- GetTop("http://www.wsj.com/xml/rss/3_7031.xml")
 newsData <- ProcessTop("wsj.com", xmltop, "//rss/channel/item", "pubDate", "title", "description", "link", FALSE)
 aData <- rbind(aData, newsData)
 
+head(aData)
+
 ####################
 ### PROCESS DATA ###
 ####################
 
-# Create non-factor data
-aData[,"siteName"] <- levels(aData[,"siteName"])[aData[,"siteName"]]
-aData[,"timeStamp"] <- levels(aData[,"timeStamp"])[aData[,"timeStamp"]]
-aData[,"title"] <- levels(aData[,"title"])[aData[,"title"]]
 aData[,"title"] <- gsub("'", "", aData[,"title"])
-# aData[,"description"] <- levels(aData[,"description"])[aData[,"description"]]
 aData[,"description"] <- gsub("'", "", aData[,"description"])
-aData[,"link"] <- levels(aData[,"link"])[aData[,"link"]]
-aData[,"tickers"] <- levels(aData[,"tickers"])[aData[,"tickers"]]
 aData[,"timeStamp"] <- GetTime(aData[,"timeStamp"])
-
 aData <- aData[order(aData[,"timeStamp"], decreasing=TRUE), ]
 
 print(head(aData))
